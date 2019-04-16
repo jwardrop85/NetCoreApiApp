b8bd6910d0a9bc448d0be6e9711099d3ebaa431e
John Wardrop <johnwardrop@outlook.com>
2019-04-16 23:43:23 +0100

    health controller

---
+++ NetCoreApiApp/Controllers/HealthController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        // GET: api/Health
+        [HttpGet]
+        public string Get()
+        {
+            return "200 OK";
+        }
+
+        // GET: api/Health/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Health
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+    }
+}
